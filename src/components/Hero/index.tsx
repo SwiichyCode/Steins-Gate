8e201665fbc3b6926c0a6bab1b820be8bea55491
Swiichy <adlpromail@gmail.com>
2023-07-01 05:27:53 +0200

    fix: escape character

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,12 +3,12 @@ import * as S from "./styles";
 export default function Hero() {
   return (
     <S.Container>
-      <h1>Rejoins l'aventure.</h1>
+      <h1>Rejoins l&apos;aventure.</h1>
       <p>
-        Prêt à vivre l'aventure ultime ? Rejoins notre guilde et deviens le
-        protagoniste de ton propre récit héroïque. Clique sur le bouton "Nous
-        rejoindre" et embarque dès maintenant pour des combats épiques, des
-        quêtes passionnantes et des camarades de jeu dévoués.
+        Prêt à vivre l&apos;aventure ultime ? Rejoins notre guilde et deviens le
+        protagoniste de ton propre récit héroïque. Clique sur le bouton
+        &ldquo;Nous rejoindre&rdquo; et embarque dès maintenant pour des combats
+        épiques, des quêtes passionnantes et des camarades de jeu dévoués.
       </p>
       <button>Nous rejoindre !</button>
     </S.Container>
