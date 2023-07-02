23fbfef64b3fe30bf279d12045a240f9917b692b
Swiichy <adlpromail@gmail.com>
2023-07-02 11:39:55 +0200

    feat: add animation on link

--- src/components/AuthButton/index.tsx
+++ src/components/AuthButton/index.tsx
@@ -28,6 +28,11 @@ const Container = styled.button<{ backgroundcolor: string }, { color: string }>`
   background-color: ${({ backgroundcolor }) => backgroundcolor};
   padding: 0 20px;
   cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: 0px 10px 42px -3px rgba(220, 20, 60, 0.7);
+  }
 
   a {
     text-decoration: none;
--- src/components/Navigation/styles.ts
+++ src/components/Navigation/styles.ts
@@ -11,8 +11,39 @@ export const Container = styled.nav`
   }
 
   a {
+    position: relative;
     text-decoration: none;
     letter-spacing: 1px;
     color: #f8f7f9;
+
+    &::after {
+      content: "";
+      display: block;
+      height: 0.2rem;
+      background: #f8f7f9;
+      transform: scale(0);
+      transition: transform 0.6s;
+      transform-origin: right;
+      animation: fade-in 0.6s;
+    }
+
+    &:hover::after {
+      transform: scale(1);
+      transform-origin: left;
+    }
+
+    @keyframes fade-in {
+      0% {
+        opacity: 0;
+      }
+
+      90% {
+        opacity: 0.6;
+      }
+
+      100% {
+        opacity: 1;
+      }
+    }
   }
 `;
