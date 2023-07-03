517663266ebb7046078b802fb4339d7133b41f28
Swiichy <adlpromail@gmail.com>
2023-07-03 18:53:03 +0200

    .

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import API_URL from "@/constants/api_url";
 
-const fromLocalHost = true;
+const fromLocalHost = false;
 
 const api_url = fromLocalHost
   ? "http://localhost:8080/api/auth/"
