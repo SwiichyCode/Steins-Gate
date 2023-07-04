c7d1c8d060527b63d36bdbd851da3a5c1dd4eee4
Swiichy <adlpromail@gmail.com>
2023-07-04 08:05:05 +0200

    feat: add role constant

--- src/layouts/ProtectedAdminLayout.tsx
+++ src/layouts/ProtectedAdminLayout.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { PAGE_URL } from "@/constants/page_url";
+import { ROLE } from "@/constants/role";
 
 type Props = {
   children: React.ReactNode;
@@ -10,10 +11,10 @@ export default function ProtectedAdminLayout({ children }: Props) {
   const router = useRouter();
 
   useEffect(() => {
-    const user = localStorage.getItem("user");
+    const user = localStorage.getItem(ROLE.USER);
     const role = user ? JSON.parse(user).roles : null;
 
-    if (!user || role[0] !== "admin") {
+    if (!user || role[0] !== ROLE.ADMIN) {
       router.push(PAGE_URL.HOME);
     }
   }, [router]);
--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { PAGE_URL } from "@/constants/page_url";
+import { ROLE } from "@/constants/role";
 
 type Props = {
   children: React.ReactNode;
@@ -10,7 +11,7 @@ export default function ProtectedLayout({ children }: Props) {
   const router = useRouter();
 
   useEffect(() => {
-    const user = localStorage.getItem("user");
+    const user = localStorage.getItem(ROLE.USER);
     if (!user) {
       router.push(PAGE_URL.LOGIN);
     }
