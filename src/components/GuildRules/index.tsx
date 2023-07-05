cd466e6b371d935dbc19c790efcf016bdd52e904
Swiichy <adlpromail@gmail.com>
2023-07-05 19:31:04 +0200

    fix: escaped character

--- src/components/GuildRules/index.tsx
+++ src/components/GuildRules/index.tsx
@@ -33,10 +33,11 @@ export default function GuildRules() {
       <p>Bonjour et bienvenue à toi,</p>
       <p>
         Voici quelques lignes à lire avant de te décider à postuler ou pas dans
-        notre guilde et communauté Steins Gate. Notre esprit de guilde n'est pas
-        une prise de tête. Nous acceptons que le fait que chaque personne a un
-        gameplay différent et qui lui propre. Il/Elle est libre de jouer à son
-        rythme sans obligation sauf pour les membres du raid inscrits.
+        notre guilde et communauté Steins Gate. Notre esprit de guilde
+        n&apos;est pas une prise de tête. Nous acceptons que le fait que chaque
+        personne a un gameplay différent et qui lui propre. Il/Elle est libre de
+        jouer à son rythme sans obligation sauf pour les membres du raid
+        inscrits.
       </p>
       <p>
         Afin que tous le monde passe un bon moment, nous te demandons de lire
@@ -45,13 +46,13 @@ export default function GuildRules() {
       <ol>
         <li>
           - Le respect des autres joueurs que ce soit sur le plan du
-          gameplay(tout le monde n'a pas les même capacités), de l'identité,
-          séxualité, ect...
+          gameplay(tout le monde n&apos;a pas les même capacités), de
+          l&apos;identité, séxualité, ect...
         </li>
         <li>
           - Du sérieux en vocal lors des raids. Bien que la guilde ne soit pas
-          du tryhard, c'est plus cool de garder un peu de sérieux durant ces
-          soirées.
+          du tryhard, c&apos;est plus cool de garder un peu de sérieux durant
+          ces soirées.
         </li>
         <li>
           - Privilégie si possible les membres de ta guilde au PU si tu as des
@@ -59,15 +60,15 @@ export default function GuildRules() {
         </li>
         <li>
           - Si tu est absent pour le raid ou pour une durée plus longue,
-          préviens nous dés que possible que l'on puisse s'organiser. Par
-          ailleurs, si un soir tu as la flemme de raid, pas de soucis ! Préviens
-          juste ton abscence.
+          préviens nous dés que possible que l&apos;on puisse s&apos;organiser.
+          Par ailleurs, si un soir tu as la flemme de raid, pas de soucis !
+          Préviens juste ton abscence.
         </li>
         <li>
-          - Afin d'atteindre tes objectifs, l'optimisation du perso est
-          importante. On connaît la RNG pour loot du bon stuff sur LA, on sera
-          compréhensif mais on évite de venir avec aucune gravure ou pire...
-          avec des bijoux "domination".
+          - Afin d&apos;atteindre tes objectifs, l&apos;optimisation du perso
+          est importante. On connaît la RNG pour loot du bon stuff sur LA, on
+          sera compréhensif mais on évite de venir avec aucune gravure ou
+          pire... avec des bijoux &quot;domination&quot;.
         </li>
         <li>
           - Lors des raids, pense a prévoir ton stock de potions, bombes,
